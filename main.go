b7404ac9252a32838df244d53cd1141a4015ec04
David Fischer <david@fisch3r.net>
2024-05-30 15:04:31 +0200

    Fix fmt

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 	"flag"
 	"log"
 
-	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/davidfischer-ch/terraform-provider-aria/internal/provider"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
 // Run "go generate" to format example terraform files and generate the docs for the registry/website
