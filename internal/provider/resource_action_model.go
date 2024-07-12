bffa524de9bc91d95797d0a668660f4b2e16cb10
David Fischer <david@fisch3r.net>
2024-07-12 13:29:42 +0200

    Fix ResourceActionModel.String()

--- internal/provider/resource_action_model.go
+++ internal/provider/resource_action_model.go
@@ -47,7 +47,7 @@ type ResourceActionAPIModel struct {
 
 func (self *ResourceActionModel) String() string {
 	return fmt.Sprintf(
-		"Custom Resource %s Additional Action %s (%s) project %s",
+		"%s Additional Action %s (%s) project %s",
 		self.ResourceType.ValueString(),
 		self.Id.ValueString(),
 		self.Name.ValueString(),
