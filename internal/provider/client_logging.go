ac4ab8c033e3459317ee788b2200f5793137442f
David Fischer <david@fisch3r.net>
2025-02-17 16:10:37 +0100

    Fix linter issue

--- internal/provider/client_logging.go
+++ internal/provider/client_logging.go
@@ -23,13 +23,13 @@ func (self AriaClient) Trace(message string, args ...any) {
 
 func (self AriaClient) Log(level string, message string, args ...any) {
 	if level == "DEBUG" {
-		self.Debug(message, args)
+		self.Debug(message, args...)
 	} else if level == "INFO" {
-		self.Info(message, args)
+		self.Info(message, args...)
 	} else if level == "TRACE" {
-		self.Trace(message, args)
+		self.Trace(message, args...)
 	} else {
 		self.Debug("Unknown log level %s, defaulting to TRACE", level)
-		self.Trace(message, args)
+		self.Trace(message, args...)
 	}
 }
