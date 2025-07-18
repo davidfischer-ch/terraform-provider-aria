dd9c11453ee361a80a96cba423bbe2b238229583
Fischer David (DIN) <david.fischer@etat.ge.ch>
2025-07-18 15:52:47 +0200

    Add import_trigger to aria_catalog_source

--- examples/resources/aria_catalog_source/resource.tf
+++ examples/resources/aria_catalog_source/resource.tf
@@ -22,75 +22,6 @@ resource "aria_catalog_source" "library_project_cloud_templates" {
 
 # Create a Workflow and make it available using a Catalog Source -----------------------------------
 
-# Method 1 - Using only the catalog source's waiting mechanism
-
-# data.tf
-
-data "aria_integration" "workflows" {
-  type_id = "com.vmw.vro.workflow"
-}
-
-# main.tf
-
-resource "aria_orchestrator_category" "my_company" {
-  name      = "MyCompany"
-  type      = "WorkflowCategory"
-  parent_id = ""
-}
-
-# Example is dummy and contains no code
-resource "aria_orchestrator_workflow" "dummy" {
-  name        = "Dummy Workflow for Catalog Source"
-  description = "Workflows doing nothing particular."
-  category_id = aria_orchestrator_category.my_company.id
-  version     = "0.1.0"
-
-  position = { x = 100, y = 50 }
-
-  restart_mode            = 1 # resume
-  resume_from_failed_mode = 0 # default
-
-  attrib        = jsonencode([])
-  presentation  = jsonencode({})
-  workflow_item = jsonencode([])
-
-  input_parameters  = []
-  output_parameters = []
-
-  input_forms = jsonencode([
-    {
-      layout = {
-        pages = []
-      }
-      schema = {}
-    }
-  ])
-}
-
-resource "aria_catalog_source" "dummy" {
-  name        = "Dummy Workflow Catalog Source"
-  description = "Publish the dummy workflow."
-  type_id     = data.aria_integration.workflows.type_id
-
-  config = {
-    workflows = [
-      {
-        id          = aria_orchestrator_workflow.dummy.id
-        name        = aria_orchestrator_workflow.dummy.name
-        description = aria_orchestrator_workflow.dummy.description
-        version     = aria_orchestrator_workflow.dummy.version
-        integration = {
-          name                        = data.aria_integration.workflows.name
-          endpoint_configuration_link = data.aria_integration.workflows.endpoint_configuration_link
-          endpoint_uri                = data.aria_integration.workflows.endpoint_uri
-        }
-      }
-    ]
-  }
-}
-
-# Create a Workflow and make it available using a Catalog Source -----------------------------------
-
 # Method 1
 #
 # Using only the catalog source's waiting mechanism.
@@ -163,6 +94,9 @@ resource "aria_catalog_source" "dummy" {
       }
     ]
   }
+
+  # Refresh the catalog source every time the workflow is changed
+  import_trigger = aria_orchestrator_workflow.dummy.version_id
 }
 
 # Create a Workflow and make it available using a Catalog Source -----------------------------------
@@ -226,4 +160,7 @@ resource "aria_catalog_source" "dummy" {
       }
     ]
   }
+
+  # Refresh the catalog source every time the workflow is changed
+  import_trigger = aria_orchestrator_workflow.dummy.version_id
 }
