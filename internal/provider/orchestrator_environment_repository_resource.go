bc298acb1207308f1860a0f164ac6f7fabafc7f2
David Fischer <david@fisch3r.net>
2026-03-03 15:48:03 +0100

    Add generic CRUD resource to deduplicate standard resources

--- internal/provider/orchestrator_environment_repository_resource.go
+++ internal/provider/orchestrator_environment_repository_resource.go
@@ -3,149 +3,21 @@
 
 package provider
 
-import (
-	"context"
-	"fmt"
-
-	"github.com/hashicorp/terraform-plugin-framework/path"
-	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
-)
-
-// Ensure provider defined types fully satisfy framework interfaces.
-var _ resource.Resource = &OrchestratorEnvironmentRepositoryResource{}
-var _ resource.ResourceWithImportState = &OrchestratorEnvironmentRepositoryResource{}
+import "github.com/hashicorp/terraform-plugin-framework/resource"
 
 func NewOrchestratorEnvironmentRepositoryResource() resource.Resource {
-	return &OrchestratorEnvironmentRepositoryResource{}
-}
-
-// OrchestratorEnvironmentRepositoryResource defines the resource implementation.
-type OrchestratorEnvironmentRepositoryResource struct {
-	client *AriaClient
-}
-
-func (self *OrchestratorEnvironmentRepositoryResource) Metadata(
-	ctx context.Context,
-	req resource.MetadataRequest,
-	resp *resource.MetadataResponse,
-) {
-	resp.TypeName = req.ProviderTypeName + "_orchestrator_environment_repository"
-}
-
-func (self *OrchestratorEnvironmentRepositoryResource) Schema(
-	ctx context.Context,
-	req resource.SchemaRequest,
-	resp *resource.SchemaResponse,
-) {
-	resp.Schema = OrchestratorEnvironmentRepositorySchema()
-}
-
-func (self *OrchestratorEnvironmentRepositoryResource) Configure(
-	ctx context.Context,
-	req resource.ConfigureRequest,
-	resp *resource.ConfigureResponse,
-) {
-	self.client = GetResourceClient(ctx, req, resp)
-}
-
-func (self *OrchestratorEnvironmentRepositoryResource) Create(
-	ctx context.Context,
-	req resource.CreateRequest,
-	resp *resource.CreateResponse,
-) {
-	// Read Terraform plan data into the model
-	var repository OrchestratorEnvironmentRepositoryModel
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &repository)...)
-	if resp.Diagnostics.HasError() {
-		return
+	return &SimpleGenericResource[
+		OrchestratorEnvironmentRepositoryModel,
+		*OrchestratorEnvironmentRepositoryModel,
+		OrchestratorEnvironmentRepositoryAPIModel,
+	]{
+		config: GenericResourceConfig{
+			TypeName:    "_orchestrator_environment_repository",
+			SchemaFunc:  OrchestratorEnvironmentRepositorySchema,
+			UpdateCodes: []int{202},
+			ImportStateSetAttributes: map[string]string{
+				"system_credentials": "",
+			},
+		},
 	}
-
-	var repositoryFromAPI OrchestratorEnvironmentRepositoryAPIModel
-	_, createDiags := self.client.CreateIt(&repository, &repositoryFromAPI, repository.ToAPI())
-	resp.Diagnostics.Append(createDiags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	// Save repository into Terraform state
-	repository.FromAPI(repositoryFromAPI)
-	resp.Diagnostics.Append(resp.State.Set(ctx, &repository)...)
-	tflog.Debug(ctx, fmt.Sprintf("Created %s successfully", repository.String()))
-}
-
-func (self *OrchestratorEnvironmentRepositoryResource) Read(
-	ctx context.Context,
-	req resource.ReadRequest,
-	resp *resource.ReadResponse,
-) {
-	// Read Terraform prior state data into the model
-	var repository OrchestratorEnvironmentRepositoryModel
-	resp.Diagnostics.Append(req.State.Get(ctx, &repository)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	var repositoryFromAPI OrchestratorEnvironmentRepositoryAPIModel
-	found, _, readDiags := self.client.ReadIt(&repository, &repositoryFromAPI)
-	resp.Diagnostics.Append(readDiags...)
-	if !found {
-		resp.State.RemoveResource(ctx)
-		return
-	}
-
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	// Save updated repository into Terraform state
-	repository.FromAPI(repositoryFromAPI)
-	resp.Diagnostics.Append(resp.State.Set(ctx, &repository)...)
-}
-
-func (self *OrchestratorEnvironmentRepositoryResource) Update(
-	ctx context.Context,
-	req resource.UpdateRequest,
-	resp *resource.UpdateResponse,
-) {
-	// Read Terraform plan data into the model
-	var repository OrchestratorEnvironmentRepositoryModel
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &repository)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	var repositoryFromAPI OrchestratorEnvironmentRepositoryAPIModel
-	_, updateDiags := self.client.UpdateIt(&repository, &repositoryFromAPI, repository.ToAPI(), "PUT", 202)
-	resp.Diagnostics.Append(updateDiags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	// Save updated repository into Terraform state
-	repository.FromAPI(repositoryFromAPI)
-	resp.Diagnostics.Append(resp.State.Set(ctx, &repository)...)
-	tflog.Debug(ctx, fmt.Sprintf("Updated %s successfully", repository.String()))
-}
-
-func (self *OrchestratorEnvironmentRepositoryResource) Delete(
-	ctx context.Context,
-	req resource.DeleteRequest,
-	resp *resource.DeleteResponse,
-) {
-	// Read Terraform prior state data into the model
-	var repository OrchestratorEnvironmentRepositoryModel
-	resp.Diagnostics.Append(req.State.Get(ctx, &repository)...)
-	if !resp.Diagnostics.HasError() {
-		resp.Diagnostics.Append(self.client.DeleteIt(&repository)...)
-	}
-}
-
-func (self *OrchestratorEnvironmentRepositoryResource) ImportState(
-	ctx context.Context,
-	req resource.ImportStateRequest,
-	resp *resource.ImportStateResponse,
-) {
-	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
-	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("system_credentials"), "")...)
 }
